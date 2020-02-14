a84fd093b98ff626baee798dd21b200b13374970
Kyle Mitofsky <kylemit@gmail.com>
2020-02-14 13:33:39 -0500

    add npm init, update readme, use async, add debugger

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 // for my one true love, Kyle
-// npm install require cheerio axios nodemailer twilio dotenv
-
 
 // environment variables
 require('dotenv').config()
@@ -22,49 +20,57 @@ const userPhoneNumber = "8027936498"
 
 main()
 
-function main() {
+async function main() {
 
     // one time use for initial notification of tracking
     // notifyUserEnrolled()
 
     // check Best Buy
-    let bestBuyURL = "https://www.bestbuy.com/site/lg-gram-2-in-1-14-touch-screen-laptop-intel-core-i7-16gb-memory-1tb-ssd-dark-silver/6398013.p?skuId=6398013"
-    loadHTML(bestBuyURL).then((returnedHTML) => {
-        // creates metaphor for accessing DOM
-        const $ = cheerio.load(returnedHTML)
-
-        // product button state (either disabled or not disabled)
-        let comingSoonButtonText = $(".fulfillment-add-to-cart-button button").text()
-
-        // notify condition
-        if (comingSoonButtonText != "Coming Soon") {
-            let productName = $(".sku-title").text().trim()
-            let location = "Best Buy"
-            notifyAvailability(location, productName, bestBuyURL)
-        }
-    })
-
-    let amazonURL = "https://www.amazon.com/dp/B083CCRR5W"
-    loadHTML(amazonURL).then((returnedHTML) => {
-        // creates metaphor for accessing DOM
-        const $ = cheerio.load(returnedHTML)
-
-        // locates availability status
-        let unavailableElement = $("#availability").children().first()
-        // gets text value and trims whitespace chars
-        unavailableElement = unavailableElement.text().trim()
-
-
-        // notify condition
-        if (unavailableElement != "Currently unavailable.") {
-            let productName = $("#productTitle").text().trim()
-            let location = "Amazon"
-            notifyAvailability(location, productName, amazonURL)
-        }
-    })
-
+    await checkBestBuy()
+    await checkAmazon()
 }
 
+
+async function checkBestBuy() {
+
+    let bestBuyURL = "https://www.bestbuy.com/site/lg-gram-2-in-1-14-touch-screen-laptop-intel-core-i7-16gb-memory-1tb-ssd-dark-silver/6398013.p?skuId=6398013"
+    let bestBuyHtml = await loadHTML(bestBuyURL)
+    
+    // creates metaphor for accessing DOM
+    const $ = cheerio.load(bestBuyHtml)
+
+    // product button state (either disabled or not disabled)
+    let comingSoonButtonText = $(".fulfillment-add-to-cart-button button").text()
+
+    // notify condition
+    if (comingSoonButtonText != "Coming Soon") {
+        let productName = $(".sku-title").text().trim()
+        let location = "Best Buy"
+        notifyAvailability(location, productName, bestBuyURL)
+    }
+}
+
+async function checkAmazon() {
+    
+    let amazonURL = "https://www.amazon.com/dp/B083CCRR5W"
+    let returnedHTML = await loadHTML(amazonURL)
+    // creates metaphor for accessing DOM
+    const $ = cheerio.load(returnedHTML)
+
+    // locates availability status
+    let unavailableElement = $("#availability").children().first()
+    // gets text value and trims whitespace chars
+    unavailableElement = unavailableElement.text().trim()
+
+
+    // notify condition
+    if (unavailableElement != "Currently unavailable.") {
+        let productName = $("#productTitle").text().trim()
+        let location = "Amazon"
+        notifyAvailability(location, productName, amazonURL)
+    }
+
+}
 
 
 async function loadHTML(url) {
